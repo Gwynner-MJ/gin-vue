3ac2feae0dbe077a1f9c7dbcc7def613f4a0e94d
Gwynner-MJ <mjn9520@gmail.com>
2022-03-18 19:12:04 +0800

    后端完1.0

--- main.go
+++ main.go
@@ -2,15 +2,41 @@ package main
 
 import (
 	"gin-vue/commen"
+	"log"
+	"os"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/spf13/viper"
 )
 
 func main() {
+	InitConfig()
 	db := commen.InitDB()
 	defer db.Close()
 	r := gin.Default()
 	r = CollectRoute(r)
+	port := viper.GetString("server.port")
+	if port != "" {
+		panic(r.Run(":" + port))
+	}
 	panic(r.Run())
+}
+
+func InitConfig() {
+	workDir, _ := os.Getwd()
+	viper.SetConfigName("application")
+	viper.SetConfigType("yml")
+	viper.AddConfigPath(workDir + "/config")
+	err := viper.ReadInConfig()
+	if err != nil {
+		if _, ok := err.(viper.ConfigFileNotFoundError); ok {
+			// Config file not found; ignore error if desired
+			log.Println("no such config file")
+		} else {
+			// Config file was found but another error was produced
+			log.Println("read config error")
+		}
+		log.Fatal(err)
+	}
 }
